7329a8e157c15bd2c803e0a0c2d6bee1bd072545
Cad Bane <faizov@gmail.com>
2021-07-10 11:55:47 -0700

    Direct scan into struct

--- DatabasePG.go
+++ DatabasePG.go
@@ -3,8 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
-
+	sqlextention "github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 const (
@@ -41,6 +42,7 @@ func NewPGDB() (*PgDB, error) {
 }
 
 func (db *PgDB) Add(newHistoryElement HistoryElement) error {
+
 	//Trying to add new element in DB
 
 	//Creating SQL request that will insert ne element id TestTable - new element is a request we processed
@@ -79,42 +81,66 @@ func (db *PgDB) Delete(id int32) error {
 
 func (db *PgDB) GetHistory(offset, limit int) ([]*historyCopyElement, error) {
 
-	historyCopy := make([]*historyCopyElement, 0)
+	pgConnection := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db2, err := sqlextention.Connect("postgres", pgConnection)
+	if err != nil {
+		return nil, err
+	}
 
 	if limit == 0 {
 		limit = 2147483647
 	}
-	//we request required data from testTable
-	requests, err := db.Database.Query(`
+
+	// first request to count number of elements in table
+	numberOfRequests := db2.QueryRowx(`
+		Select COUNT(*) from(
+			SELECT * FROM requests 
+			ORDER by time ASC
+			LIMIT $1 OFFSET $2
+		)as listofrows
+		`, limit, offset)
+	var rowsNumber int
+	numberOfRequests.Scan(&rowsNumber)
+
+	//Creating slice for final results
+	historyCopy := make([]*historyCopyElement, rowsNumber)
+
+	// second request to get data from table
+	requests, err := db2.Queryx(`
 		SELECT * FROM requests 
 		ORDER by time ASC
 		LIMIT $1 OFFSET $2
 	`, limit, offset)
-
 	if err != nil {
 		return nil, err
 	}
 
 	//We go over result of our request  going line by line
+	elementNumberInSlice := 0
 	for requests.Next() {
 
-		elementFromTable := &historyCopyElement{}
+		//elementFromTable := &historyCopyElement{}
+		if elementNumberInSlice <= rowsNumber {
+			log.Println(elementNumberInSlice)
+			historyCopy[elementNumberInSlice] = &historyCopyElement{}
+			err = requests.Scan(
+				&historyCopy[elementNumberInSlice].ID,
+				&historyCopy[elementNumberInSlice].Element.Request.Url,
+				&historyCopy[elementNumberInSlice].Element.Request.Method,
+				&historyCopy[elementNumberInSlice].Element.Request.Body,
+				&historyCopy[elementNumberInSlice].Element.Time,
+				&historyCopy[elementNumberInSlice].Element.Respond.HttpStatusCode,
+				&historyCopy[elementNumberInSlice].Element.Respond.ContentLength,
+			)
 
-		err = requests.Scan(
-			&elementFromTable.ID,
-			&elementFromTable.Element.Request.Url,
-			&elementFromTable.Element.Request.Method,
-			&elementFromTable.Element.Request.Body,
-			&elementFromTable.Element.Time,
-			&elementFromTable.Element.Respond.HttpStatusCode,
-			&elementFromTable.Element.Respond.ContentLength,
-		)
-		if err != nil {
-			return historyCopy, err
+			if err != nil {
+				return historyCopy, err
+			}
+			elementNumberInSlice++
 		}
 
-		//adding each element into whole history in our format
-		historyCopy = append(historyCopy, elementFromTable)
 	}
 
 	return historyCopy, nil
